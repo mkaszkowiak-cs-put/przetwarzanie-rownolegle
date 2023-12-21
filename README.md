b6d8c78ad5a77ab5507b475e17094984d7b84402
energywraith <energywraith@gmail.com>
2023-12-21 15:46:52 +0100

    Koncepcyjne - Testowanie każdej liczby

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # przetwarzanie-rownolegle
 Projekt OMP do Walkusia
+
+## Uruchomienie kodu:
+
+```code
+cd koncepcyjne
+```
+
+```code
+g++ .\1_greedy.cpp -static-libstdc++ -o 1_greedy.exe
+```
+
+```code
+.\1_greedy.exe <start> <end>
+```
---
+++ koncepcyjne/1_greedy.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include "../measure_time.cpp"
+
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    if (num == 2) return true;
+    if (num % 2 == 0) return false;
+    
+    for (int i = 3; i <= sqrt(num); i += 2) {
+        if (num % i == 0) return false;
+    }
+
+    return true;
+}
+
+int getPrimesSize(int start, int end) {
+    std::vector<int> primes;
+
+    for (int i = start; i <= end; i++) {
+        if (isPrime(i)) {
+            primes.push_back(i);
+        }
+    }
+
+    return primes.size();
+}
+
+int main(int argc, char *argv[]) {
+    int start = 2;
+    int end = 20000000;
+
+    if(argc == 3) {
+        start = atoi(argv[1]);
+        end = atoi(argv[2]);
+    }
+
+    measureTime("Po kolei", getPrimesSize, start, end);
+    return 0;
+}
